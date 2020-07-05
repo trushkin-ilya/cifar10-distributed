616a639bfe0debd0b23b8a62c2650b911a032901
trushkin-ilya <viciousde@gmail.com>
2020-07-05 15:23:15 +0300

    move training to tf.estimator

--- models.py
+++ models.py
@@ -1,21 +1,32 @@
+import horovod.tensorflow as hvd
 import tensorflow as tf
 
-def conv_model(feature, target, mode):
-    target = tf.one_hot(tf.cast(target, tf.int32), 10, 1, 0)
-    feature = tf.reshape(feature, (-1, 32, 32, 3))
-    with tf.variable_scope('conv_layer1'):
-        h_conv1 = tf.layers.conv2d(feature, 32, kernel_size=[5, 5], activation=tf.nn.relu, padding="SAME")
-        h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    with tf.variable_scope('conv_layer2'):
-        h_conv2 = tf.layers.conv2d(h_pool1, 64, kernel_size=[5, 5], activation=tf.nn.relu, padding="SAME")
-        h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
+def cnn_model_fn(features, labels, mode):
+    conv1 = tf.layers.conv2d(inputs=features['x'], filters=32, kernel_size=[5, 5], padding="same", activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    conv2 = tf.layers.conv2d(inputs=pool1, filters=64, kernel_size=[5, 5], padding="same", activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    pool2_flat = tf.reshape(pool2, [-1, 8 * 8 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+    logits = tf.layers.dense(inputs=dropout, units=10)
 
-    h_fc1 = tf.layers.dropout(tf.layers.dense(h_pool2_flat, 1024, activation=tf.nn.relu), rate=0.5,
-                           training=mode == tf.estimator.ModeKeys.TRAIN)
+    predictions = {
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    logits = tf.layers.dense(h_fc1, 10, activation=None)
-    loss = tf.losses.softmax_cross_entropy(target, logits)
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10, axis=-1)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
 
-    return tf.argmax(logits, 1), loss
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.MomentumOptimizer(learning_rate=0.001 * hvd.size(), momentum=0.9)
+        optimizer = hvd.DistributedOptimizer(optimizer)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
--- train.py
+++ train.py
@@ -1,10 +1,13 @@
+import os
 from argparse import ArgumentParser
+from datetime import datetime
 
 import horovod.tensorflow as hvd
 import numpy as np
 import tensorflow as tf
+
 from load_data import load_data
-from models import conv_model
+from models import cnn_model_fn
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -19,21 +22,9 @@ argparser.add_argument("--use-gpu", type=bool, default=False)
 args = argparser.parse_args()
 
 
-def train_input_generator(x_train, y_train, batch_size):
-    assert len(x_train) == len(y_train)
-    while True:
-        p = np.random.permutation(len(x_train))
-        x_train, y_train = x_train[p], y_train[p]
-        index = 0
-        while index <= len(x_train) - batch_size:
-            yield x_train[index:index + batch_size], \
-                  y_train[index:index + batch_size],
-            index += batch_size
-
-
 def main(_):
     hvd.init()
-    master_node = hvd.rank() == 0
+    chief = hvd.rank() == 0
     num_workers = hvd.size()
 
     config = tf.ConfigProto()
@@ -41,33 +32,28 @@ def main(_):
         config.gpu_options.allow_growth = True
         config.gpu_options.visible_device_list = str(hvd.local_rank())
 
-    with tf.name_scope('input'):
-        image = tf.placeholder(tf.float32, [None, 1024, 3], name='image')
-        label = tf.placeholder(tf.float32, [None], name='label')
-    predict, loss = conv_model(image, label, tf.estimator.ModeKeys.TRAIN)
-    optimizer = hvd.DistributedOptimizer(tf.train.MomentumOptimizer(learning_rate=args.lr * num_workers, momentum=0.9))
-    global_step = tf.train.get_or_create_global_step()
-    train_op = optimizer.minimize(loss, global_step=global_step)
-
     (x_train, y_train), (x_test, y_test) = load_data(args.data_dir)
-    x_train, y_train = x_train[hvd.rank()::hvd.size()], y_train[hvd.rank()::hvd.size()]
-    order = np.random.permutation(len(x_train))
-    x_train, y_train = x_train[order], y_train[order]
-    x_train = np.random.permutation(x_train)
-    x_train = np.reshape(x_train, (-1, 1024, 3)) / 255.0
-    x_test = np.reshape(x_test, (-1, 1024, 3)) / 255.0
-    training_batch_generator = train_input_generator(x_train, y_train, args.batch_size)
+    x_train, y_train = x_train[hvd.rank()::num_workers], y_train[hvd.rank()::num_workers]
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
 
-    hooks = [hvd.BroadcastGlobalVariablesHook(0),
-             tf.train.StopAtStepHook(last_step=args.epochs * len(x_train) // args.batch_size),
-             tf.train.LoggingTensorHook(tensors={'step': global_step, 'loss': loss}, every_n_iter=100)]
+    hooks = [hvd.BroadcastGlobalVariablesHook(0)]
 
-    ckpt_dir = args.save_dir if master_node else None
+    model_dir = os.path.join(args.save_dir, datetime.now().strftime("%Y%m%d-%H%M%S")) if chief else None
 
-    with tf.train.MonitoredTrainingSession(checkpoint_dir=ckpt_dir, config=config, hooks=hooks) as mon_sess:
-        while not mon_sess.should_stop():
-            image_, label_ = next(training_batch_generator)
-            mon_sess.run(train_op, feed_dict={image: image_, label: np.squeeze(label_)})
+    estimator = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir=model_dir,
+                                       config=tf.estimator.RunConfig(session_config=config))
+
+    for _ in range(args.epochs):
+        train_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_train}, y=np.squeeze(y_train),
+                                                      batch_size=args.batch_size,
+                                                      num_epochs=10, shuffle=True)
+        train_spec = tf.estimator.TrainSpec(input_fn=train_fn, max_steps=len(x_train) // args.batch_size, hooks=hooks)
+
+        eval_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_test}, y=np.squeeze(y_test), num_epochs=1, shuffle=False)
+        eval_spec = tf.estimator.EvalSpec(input_fn=eval_fn)
+
+        tf.estimator.train_and_evaluate(estimator, train_spec= train_spec, eval_spec=eval_spec)
 
 
 if __name__ == "__main__":
